1343854a70530e2ce5d107fe3be4391a5532c859
Rafelck <rafelinoxz7@gmail.com>
2019-02-24 09:35:07 +0800

    Scalable

--- resource/index.js
+++ resource/index.js
@@ -1,80 +1,254 @@
-// JavaScript Document
-$(document).ready(function() {
-var x = "x"
-var o = "o"
-var count = 0;
+document.addEventListener('DOMContentLoaded', function() {
 var o_win = 0;
 var x_win = 0;
-$('#game li').click(function(){
-  if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
-   {
-	 alert('O has won the game. Start a new game')
-	 $("#game li").text("+");
-	$("#game li").removeClass('disable')
-	$("#game li").removeClass('o')
-	$("#game li").removeClass('x')
-	$("#game li").removeClass('btn-primary')
-	$("#game li").removeClass('btn-info')
-   }
-  else if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x'))
-  {
-   alert('X wins has won the game. Start a new game')
-   $("#game li").text("+");
-	$("#game li").removeClass('disable')
-	$("#game li").removeClass('o')
-	$("#game li").removeClass('x')
-	$("#game li").removeClass('btn-primary')
-	$("#game li").removeClass('btn-info')	
-  }
-  else if (count == 9)
-  {
-	alert('Its a tie. It will restart.')
-	$("#game li").text("+");
-	$("#game li").removeClass('disable')
-	$("#game li").removeClass('o')
-	$("#game li").removeClass('x')
-	$("#game li").removeClass('btn-primary')
-	$("#game li").removeClass('btn-info')
-	count = 0
-  }
-  else if ($(this).hasClass('disable'))
-  {
-    alert('Already selected')
-  }
-  else if (count%2 == 0)
-  {
-	  count++
-	  $(this).text(o)
-      $(this).addClass('disable o btn-primary')
-	    if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
-	    {
-	   alert('O wins')
-	   count = 0
-	   o_win++
-$('#o_win').text(o_win)
-        }
-  }
-   else  
-  {
-	  count++
-    $(this).text(x)
-    $(this).addClass('disable x btn-info')
-	   if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x'))
-        {
-	 alert('X wins')
-	 count = 0
-	 x_win++
-	 $('#x_win').text(x_win)
-        }
-  }
-   });
-    $("#reset").click(function () {
-    $("#game li").text("+");
-	$("#game li").removeClass('disable')
-	$("#game li").removeClass('o')
-	$("#game li").removeClass('x')
-	$("#game li").removeClass('btn-primary')
-	$("#game li").removeClass('btn-info')
-	count = 0
-  });
-});
+	
+	var optionsButton = document.getElementById("options_submit");
+	
+	
+	optionsButton.addEventListener("click", function(){
+	
+	
+	optionsButton.innerHTML = "Reset";
+	
+	
+	function isEven(value){
+			if (value % 2 == 0) {
+					 return true;
+			} else {
+					return false;
+		};
+	};
+	
+	function isOdd(value){
+		if (value % 1 == 0) {
+			return true;
+		} else {
+			return false;
+		};
+	};
+	
+	function allSame(array) { 
+		 
+			var first = array[0];
+	
+			if (array[0] == "") {
+				return false;
+			} else {
+				return array.every(function(element) {
+						return element == first;
+				});
+			};
+	};
+	
+	
+	var customBackground = document.getElementById("boardcolor_input").value;
+	
+	
+	var boardSize = parseInt(document.getElementById("boardsize_input").value);
+	
+	
+	var gameBoard = [];
+	
+	
+	var numSquares = (boardSize * boardSize);
+	
+	
+	for (var i = 0; i < numSquares; i++) {
+		gameBoard.push(i);
+	};
+	
+	
+	document.getElementById("game").innerHTML = '<div id="board"></div>';
+	
+	
+	
+	var board = document.getElementById("board");
+	
+	
+	board.style.margin = 'auto auto';
+	
+	
+	board.style.height = (100 * boardSize) + 'px';
+	board.style.width = (100 * boardSize) + 'px';
+	
+	
+	board.style.border = 'solid 1px black';
+	
+	
+	for (var i = 0; i < numSquares; i++) {
+		board.innerHTML += '<div class="square"></div>';
+	};
+	
+	
+	var squares = document.getElementsByClassName("square");
+	
+	
+	for (var i = 0; i < numSquares; i++) {
+		
+		squares[i].style.height = '100px';
+		squares[i].style.width = '100px';
+		
+		squares[i].style.float = "left";
+		
+		squares[i].style.lineHeight = "100px";
+		
+		squares[i].setAttribute("id", i.toString());
+	};
+	
+	
+	if (numSquares % 2 !== 0) { 
+		for (var i = 0; i < numSquares; i += 2) { 
+			squares[i].style.backgroundColor = customBackground;
+		};
+	} else {
+		for (i = 0; i < numSquares; i += 1) {
+			if (isEven(i/boardSize)) {
+				for (var squareNum = i; squareNum < (i + boardSize); squareNum += 2) {
+					squares[squareNum].style.backgroundColor = customBackground;	
+				};
+			} else if (isOdd(i/boardSize)) {
+				for (var squareNum = i+1; squareNum < (i + boardSize); squareNum += 2) {
+					squares[squareNum].style.backgroundColor = customBackground;	
+				};
+			} else {
+			};
+		};
+	};
+	
+	
+	var turnIndicator = document.getElementById("turnIndicator")
+	
+	
+	turnIndicator.style.color = "black";
+	turnIndicator.innerHTML = "X's Turn";
+	
+	
+	var boardClicks = 0;
+	
+	
+	board.addEventListener("click", function() {
+	if (determineWinner()) { 
+		turnIndicator.style.color = "blue";
+		turnIndicator.innerHTML = winningPlayer[0] + ' Wins!';
+
+		if(winningPlayer[0] == "X"){
+			x_win++
+			$('#x_win').text(x_win)
+		}else if(winningPlayer[0] == "O"){
+			o_win++
+			$('#o_win').text(o_win)
+		}
+		
+	} else if (isEven(boardClicks)) {
+		turnIndicator.style.color = "red";
+		turnIndicator.innerHTML = "O's Turn";
+	} else {
+		turnIndicator.style.color = "black";
+		turnIndicator.innerHTML = "X's Turn";
+	};
+	boardClicks++;
+
+	}); 
+	var squareClicks = [];
+	
+	
+	for (var i = 0; i < numSquares; i++) {
+		squareClicks[i] = 0;
+	};
+	
+	
+	var winningPlayer;
+	
+
+	var determineWinner = function() {
+			for (i = 0; i < numSquares; i += 1) { 
+				if ((i % boardSize) == 0) {
+				var rowCheck = [];
+				for (var squareNum = i; squareNum < (i + boardSize); squareNum += 1) {	
+					rowCheck.push(squares[squareNum].innerHTML);
+				};
+				console.log('Row ' + i + ' is ' + rowCheck);
+				console.log(allSame(rowCheck));
+	
+				if (allSame(rowCheck)) {
+					winningPlayer = rowCheck; 
+					return true;
+				};
+			};
+		};
+		
+		for (i = 0; i < numSquares; i += 1) { 
+			if (i < boardSize) { 
+				var colCheck = [];
+				for (var squareNum = i; squareNum < numSquares; squareNum += boardSize) {
+					colCheck.push(squares[squareNum].innerHTML);
+				};
+				console.log('Column ' + i + 'is ' + colCheck);
+				console.log(allSame(colCheck));
+				
+				if (allSame(colCheck)) {
+					winningPlayer = colCheck; 
+					return true;
+				};	
+			};
+		};
+		var diag1Check = []; 
+		for (i = 0; i < numSquares; i += 1) { 
+						if ((i % (boardSize + 1)) == 0) {
+							console.log(i)
+				diag1Check.push(squares[i].innerHTML);
+			};
+		};
+		console.log(diag1Check)
+		console.log(allSame(diag1Check));
+		if (allSame(diag1Check)) {
+			winningPlayer = diag1Check;
+			return true;
+		};
+		
+		var diag2Check = []; 
+		for (i = (boardSize - 1); i < (numSquares - 1); i += 1) {
+			if ((i % (boardSize - 1)) == 0) { 
+				console.log(i)
+				diag2Check.push(squares[i].innerHTML);
+			};
+		};
+		console.log(diag2Check)
+		console.log(allSame(diag2Check));
+		if (allSame(diag2Check)) {
+			winningPlayer = diag2Check;
+			return true;
+		};
+	};
+	
+	
+	var countClicks = function() {
+		var divID = this.getAttribute("id");
+		squareClicks[divID] += 1;
+		
+		if (isEven(boardClicks) && squareClicks[divID] == 1) {
+			this.innerHTML = 'X';
+		} else if (isOdd(boardClicks) && squareClicks[divID] == 1) {
+			this.innerHTML = 'O';
+			this.style.color = "red";
+		} else if (!determineWinner()){
+			alert('You cannot move there. That space is taken.');
+			boardClicks -= 1;
+		} else {
+		};
+		if (determineWinner()) {
+			for (var i = 0; i < numSquares; i++) {
+				squareClicks[i] = 2;
+			};
+			document.getElementById("options_submit").innerHTML = "Play again?"
+		};
+	};
+	
+
+	for (var i = 0; i < numSquares; i++) {
+		squares[i].addEventListener("click", countClicks);
+	};
+	
+	}); 
+	
+	}); 
